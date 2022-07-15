3fc91f033f1f8469d01cf1e3db1b7c28c424d677
Maulanawesome5 <maulanawesome5@gmail.com>
2022-07-16 01:58:48 +0700

    update test/mpt_test_01.py

--- test/mpt_test_01.py
+++ test/mpt_test_01.py
@@ -9,11 +9,36 @@ import matplotlib.pyplot as plt
 import datetime as dt
 
 # Set range of time
-start_time = dt.datetime(year=2018, month=1, day=1)
-end_time = dt.datetime.now()
+start_time = dt.datetime(year=2021, month=1, day=1)
+end_time = dt.datetime(year=2022, month=1, day=3)
 
 # Read data               list ticker   data source    data periode
 test = data.DataReader(["TSLA", "AMZN"], "yahoo", start_time, end_time)
-test = test["Close"]
-print(test)
+test = test["Close"]  # ambil kolom harga closing saja
+# test = pd.DataFrame(test, copy=True)  # convert to DataFrame format
+
+# Calculate log of percentage change
+tesla = test["TSLA"].pct_change().apply(lambda x: np.log(1 + x))
+amazon = test["AMZN"].pct_change().apply(lambda x: np.log(1 + x))
+#print(tesla)
+#print(amazon)
+
+# Calculate variance of tesla stock
+var_tesla = tesla.var()
+var_amazon = amazon.var()
+print(f"Variance of TSLA stock {var_tesla}")
+print(f"Variance of AMZN stock {var_amazon}")
+
+# Calculate volatility of each stock
+tsla_vol = np.sqrt(var_tesla * 250)
+amzn_vol = np.sqrt(var_amazon * 250)
+print(f"TSLA volatility {tsla_vol}, AMZN volatility {amzn_vol}")
+
+# Volatility of both stock
+test.pct_change().apply(lambda x: np.log(1 + x)).std().apply(lambda x: x * np.sqrt(250)).plot(kind="bar")
+#plt.show()  # uncomment to preview plot diagram
+
+# covariance
+
+
 
