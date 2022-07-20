219b8e189e8f7db869f96e98b3c11af3c7964472
Maulanawesome5 <maulanawesome5@gmail.com>
2022-07-20 21:10:27 +0700

    Perintah query untuk akses database

---
+++ sql/cek_database.sql
@@ -0,0 +1,10 @@
+SHOW DATABASES;
+USE saham_indonesia;
+SHOW TABLES;
+DROP DATABASE saham_indonesia;  -- untuk hapus database beserta seluruh tabel di dalamnya
+
+-- description table info
+SHOW CREATE TABLE daftar_saham_indonesia; DESCRIBE daftar_saham_indonesia;
+
+-- select data from table
+SELECT * FROM daftar_saham_indonesia;
