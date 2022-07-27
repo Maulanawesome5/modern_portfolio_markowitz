b6220bb8ea01be59e2d72697a54cb1525db37644
Maulanawesome5 <maulanawesome5@gmail.com>
2022-07-28 00:51:28 +0700

    unit_test_01 : Drop column OHLV

--- test/prototype/unit_test_01.py
+++ test/prototype/unit_test_01.py
@@ -4,7 +4,9 @@ import pandas
 # my keyboard broken -> g, G, h, H, '_', "_"
 
 data_path = r"D:/LATIHAN PEMROGRAMAN/modern_portfolio_markowitz/data/saham_properti/APLN_master_data.xlsx"
-data_read = pandas.read_excel(data_path)
+data_read = pandas.read_excel(data_path).drop(["Open", "High", "Low", "Volume"], axis=1)
+
 df = pandas.DataFrame(data_read)
+
 
 print(df)
